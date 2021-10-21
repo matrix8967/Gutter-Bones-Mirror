9b3a9517426b2d03edbf0e0ddf61ff022bf206a8
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2021-10-21 14:12:47 -0500

    Inventory & Dot File updates.

--- files/home/Scripts/Auto.sh
+++ files/home/Scripts/Auto.sh
@@ -4,6 +4,7 @@ set -eE
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+
 DEBIAN=$(cat .Debian.txt)
 FEDORA=$(cat .Fedora.txt)
 MANJARO=$(cat .Manjaro.txt)
@@ -39,7 +40,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
+git clone https://github.com/redxtech/zsh-kitty ~/.oh-my-zsh/custom/plugins/zsh-kitty
 
 # Install Tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
