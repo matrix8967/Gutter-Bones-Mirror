dec2129939c65d6f24e985dad0536ec751f2be4b
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2021-11-02 02:07:02 -0500

    Script & Inventory Updates.

--- scripts/Auto.sh
+++ scripts/Auto.sh
@@ -301,7 +301,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/redxtech/zsh-kitty.git ~/.oh-my-zsh/custom/plugins/zsh-kitty
 
 # Install Tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
