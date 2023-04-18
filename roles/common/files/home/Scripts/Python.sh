dc5d41459bd8a0fe644338c7bf4fd64fd811bb03
Matrix <2366621-matrix8967@users.noreply.gitlab.com>
2023-04-18 05:30:35 +0000

    Feat/roles

---
+++ roles/common/files/home/Scripts/Python.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' 	# No Color
+
+pip3 install --user pywal --upgrade
+pip3 install --user shodan --upgrade
+pip3 install --user bdfr --upgrade
+pip3 install --user waybackpack --upgrade
+pip3 install --user pmbootstrap --upgrade
+pip3 install --user bpytop --upgrade
+pip3 install --user paramiko --upgrade
+pip3 install --user argcomplete --upgrade
+pip3 install --user tk --upgrade
---
+++ roles/common/files/home/Scripts/Rust.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+cargo install lsd bat bottom bandwhich mdbook procs du-dust watchexec-cli ripgrep cloak checkpwn taskwarrior-tui gpg-tui gping apkeep battop cargo-update battop petname rustscan gitui ytop watchexec-cli hx cloak
+
+sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep $(which bandwhich)
+
+# https://sn0int.readthedocs.io/en/latest/install.html
